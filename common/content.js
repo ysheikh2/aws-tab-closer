71e05b84cfe88d10971ff48533cbc684271d5777
ysheikh2 <yasser.sheikh@clariant.com>
2025-12-16 09:43:55 +0100

    add german language support

--- common/content.js
+++ common/content.js
@@ -6,6 +6,14 @@ if ((window.location.hostname === "127.0.0.1" || window.location.hostname === "l
 }
 
 // AWS VPN authentication page
-if (window.location.host === "127.0.0.1:35001" && document.body.innerText.includes("Authentication details received, processing details. You may close this window at any time.")) {
-  __BROWSER_API__.runtime.sendMessage("close-tab");
+if (window.location.host === "127.0.0.1:35001") {
+  const bodyText = document.body.innerText;
+  const successMessages = [
+    "Authentication details received, processing details. You may close this window at any time.", // English
+    "Authentifizierungsdetails empfangen, Details werden verarbeitet. Sie können dieses Fenster jederzeit schließen.", // German
+  ];
+  
+  if (successMessages.some(msg => bodyText.includes(msg))) {
+    __BROWSER_API__.runtime.sendMessage("close-tab");
+  }
 }
